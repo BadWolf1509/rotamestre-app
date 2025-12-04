f9c73ea6a721d524c2726f0e38ed41bc7b5d8399
Wellington de Oliveira <wellinton.ribeiro@v4company.com>
2025-12-04 16:23:19 -0300

    chore: remove arquivos temporários

--- tmp-ws-empty.js
+++
@@ -1,6 +0,0 @@
-const WebSocket = require('ws');
-const url = 'wss://xezslsyxjivunmhhyxtd.supabase.co/realtime/v1/websocket?apikey=&vsn=1.0.0';
-const ws = new WebSocket(url);
-ws.on('open', () => { console.log('open'); ws.close(); });
-ws.on('error', err => { console.error('error', err.message); });
-ws.on('close', (code, reason) => { console.log('close', code, reason.toString()); });
